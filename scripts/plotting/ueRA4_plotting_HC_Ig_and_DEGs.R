52b9449c82013cea79a9a513d5b44159d55fb2a7
KNGrimstad <kristoffer.nilsson.grimstad@his.se>
2022-03-15 16:48:02 +0100

    added pie chart of cells

--- scripts/plotting/ueRA4_plotting_HC_Ig_and_DEGs.R
+++ scripts/plotting/ueRA4_plotting_HC_Ig_and_DEGs.R
@@ -4,12 +4,40 @@ library(ggplot2)
 library(ggtree)
 library(clustree)
 library(gridExtra)
+library(plotrix)
+
+#######################
+###---Basic Plots---###
+#######################
+ueRA4 = readRDS("data/ueRA4_base")
+
+DimPlot(ueRA4, label = TRUE, pt.size = 2, label.size = 7, reduction = "umap",
+	cols = c("#F8766D", "#B79F00", "#00BA38", "#00BFC4", "#F564E3")) # Plot clusters in UMAP space
+
+#---Pie Chart of Cell Numbers---#
+table(Idents(ueRA4)) # Number of cells per cluster
+
+slices = c(889, 827, 713, 443, 37)
+pie(slices, labels = NA, col = c("#F8766D", "#B79F00", "#00BA38", "#00BFC4", "#F564E3"), 
+	clockwise = TRUE, main = "number of cells per cluster", cex = 3)
+
+draw.circle(0, 0, 0.35, col = "white")
+text(x = 0, y = 0, "2 909", cex = 2)
+text(x = 0.45, y = 0.35, "889", cex = 2)
+text(x = 0.2, y = -0.55, "827", cex = 2)
+text(x = -0.53, y = -0.1, "713", cex = 2)
+text(x = -0.3, y = 0.45, "443", cex = 2)
+text(x = -0.05, y = 0.9, "37", cex = 2)
+
+mylab = c("0", "1", "2", "3", "4")
+myorder = matrix(c(1, 2, 3, 4, 5), nrow = 1, ncol = 5, byrow = F)
+legend(-0.9, 1.15, mylab[myorder], cex = 1.5, 
+	fill = c("#F8766D", "#B79F00", "#00BA38", "#00BFC4", "#F564E3"), bty = "n", ncol = 5)
+
 
 ###################################
 ###---Hierarchical Clustering---###
 ###################################
-
-ueRA4 = readRDS("data/ueRA4_base")
 
 tree = BuildClusterTree(ueRA4, assay = "SCT")
 myphytree = Tool(tree, slot = "BuildClusterTree")
