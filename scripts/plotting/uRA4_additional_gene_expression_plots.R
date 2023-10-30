f3b62b2c1e77caa8bb73cf9e8a8f69d2fd9d4b9f
Kristoffer Grimstad <72564158+KNGrimstad@users.noreply.github.com>
2023-10-30 14:01:22 +0100

    Update and rename ueRA4_additional_gene_expression_plots.R to uRA4_additional_gene_expression_plots.R

--- scripts/plotting/uRA4_additional_gene_expression_plots.R
+++ scripts/plotting/uRA4_additional_gene_expression_plots.R
@@ -6,17 +6,17 @@ library(ggplot2)
 ###---Additional Gene Expression Plots---###
 ############################################
 
-ueRA4.1 = readRDS("data/ueRA4_w_subs")
+uRA4.1 = readRDS("data/uRA4_w_subs")
 
 #---PCs and Other Genes---#
-DoHeatmap(ueRA4.1, features = c("PRDM1", "IRF4", "XBP1", "IL6R", "CD79A", "SYK", 
+DoHeatmap(uRA4.1, features = c("PRDM1", "IRF4", "XBP1", "IL6R", "CD79A", "SYK", 
 	"TNFRSF1B", "ITGB2", "CD79B", "IL21R", "IFNGR1", "IFNGR2"), raster = FALSE, 
 	group.colors = c("#F8766D", "#B79F00", "#00BA38", "#00C08B", "#00B4F0", "#C77CFF", 
 	"#F564E3", "#619CFF"))
 
 #---MHC Genes---#
 
-DoHeatmap(ueRA4.1, features = c("HLA-DRA", "HLA-DRB1", "HLA-DRB5", "HLA-DPA1", "HLA-DPB1", 
+DoHeatmap(uRA4.1, features = c("HLA-DRA", "HLA-DRB1", "HLA-DRB5", "HLA-DPA1", "HLA-DPB1", 
 	"HLA-DQA1", "HLA-DQB1", "CD74", "HLA-DMA", "HLA-DMB", "HLA-DOA", "HLA-DOB", "HLA-A", 
 	"HLA-B", "HLA-C", "HLA-E", "HLA-F", "B2M", CD80", "CD86"), raster = FALSE, 
 	group.colors = c("#F8766D", "#B79F00", "#00BA38", "#00C08B", "#00B4F0", "#C77CFF", 
@@ -24,10 +24,10 @@ DoHeatmap(ueRA4.1, features = c("HLA-DRA", "HLA-DRB1", "HLA-DRB5", "HLA-DPA1", "
 
 #---Cluster 3.1 Vs. 3.0 Markers---#
 
-genes = read.xlsx("reports/ueRA4_3.1_vs_3.0_DEGs.xlsx", 
+genes = read.xlsx("reports/uRA4_3.1_vs_3.0_DEGs.xlsx", 
 	sheetName = "DEGs", col.names = TRUE, row.names = FALSE)	
 
-DoHeatmap(ueRA4.1, features = genes$gene, raster = FALSE, 
+DoHeatmap(uRA4.1, features = genes$gene, raster = FALSE, 
 	group.colors = c("#F8766D", "#B79F00", "#00BA38", "#00C08B", "#00B4F0", 
 	"#C77CFF", "#F564E3", "#619CFF")) + 
 	ggtitle("up DEGs in 3.1 vs. 3.0 (n=95)") + 
