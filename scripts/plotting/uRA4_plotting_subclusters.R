d8e3a82ba8e9134f4901a3ec8cd3ee175e91b6ac
Kristoffer Grimstad <72564158+KNGrimstad@users.noreply.github.com>
2023-10-30 14:02:36 +0100

    Update and rename ueRA4_plotting_subclusters.R to uRA4_plotting_subclusters.R

--- scripts/plotting/uRA4_plotting_subclusters.R
+++ scripts/plotting/uRA4_plotting_subclusters.R
@@ -7,9 +7,9 @@ library(ggplot2)
 ###---Basic Plots---###
 #######################
 
-ueRA4.1 = readRDS("data/ueRA4_w_subs")
+uRA4.1 = readRDS("data/uRA4_w_subs")
 
-DimPlot(ueRA4.1, label = TRUE, pt.size = 2, label.size = 7, 
+DimPlot(uRA4.1, label = TRUE, pt.size = 2, label.size = 7, 
 	cols = c("#F8766D", "#B79F00", "#00BA38", "#00C08B", "#00B4F0", "#C77CFF", "#F564E3", "#619CFF")) # Plot clusters
 
 ###################################
@@ -17,9 +17,9 @@ DimPlot(ueRA4.1, label = TRUE, pt.size = 2, label.size = 7,
 ###################################
 
 ## C3 Subclusters
-ueRA4.3 = readRDS("ueRA4_C3_subs")
+uRA4.3 = readRDS("uRA4_C3_subs")
 
-tree = BuildClusterTree(ueRA4.3, assay = "SCT")
+tree = BuildClusterTree(uRA4.3, assay = "SCT")
 myphytree = Tool(tree, slot = "BuildClusterTree")
 ggtree(myphytree) + 
 	geom_tiplab() + 
@@ -28,7 +28,7 @@ ggtree(myphytree) +
 	ggtitle("Hierarchical Clustering") # Plot dendrogram
 
 ## All clusters
-tree = BuildClusterTree(ueRA4.1, assay = "SCT")
+tree = BuildClusterTree(uRA4.1, assay = "SCT")
 myphytree = Tool(tree, slot = "BuildClusterTree")
 ggtree(myphytree) + 
 	geom_tiplab() + 
@@ -42,15 +42,15 @@ ggtree(myphytree) +
 ####################################
 
 #---Top10 Differentially Expressed Genes---#
-top10_up_degs = read.xlsx("reports/ueRA4_C3_subs.xlsx", 
+top10_up_degs = read.xlsx("reports/uRA4_C3_subs.xlsx", 
 	sheetName = "Top10_Up", col.names = TRUE, row.names = FALSE)
-top10_down_degs = read.xlsx("reports/ueRA4_C3_subs.xlsx", 
+top10_down_degs = read.xlsx("reports/uRA4_C3_subs.xlsx", 
 	sheetName = "Top10_Down", col.names = TRUE, row.names = FALSE)
 
-DoHeatmap(ueRA4.3, features = top10_up_degs$gene, raster = FALSE, 
+DoHeatmap(uRA4.3, features = top10_up_degs$gene, raster = FALSE, 
 	group.colors = c("#00C08B", "#00B4F0", "#C77CFF")) # Plot top10 up DEGs
 
-DoHeatmap(ueRA4.3, features = top10_down_degs$gene, raster = FALSE, 
+DoHeatmap(uRA4.3, features = top10_down_degs$gene, raster = FALSE, 
 	group.colors = c("#00C08B", "#00B4F0", "#C77CFF")) # Plot top10 down DEGs
 
 #---Predefined Markers---#
@@ -58,7 +58,7 @@ DoHeatmap(ueRA4.3, features = top10_down_degs$gene, raster = FALSE,
 genes = c("CR2","CD27","TBX21","ITGAX","FCRL5","FCRL3","FAS",
 	"CD19","MS4A1","SELL","CD24","CD38","CXCR5","CXCR4") # predefined markers
 
-DoHeatmap(ueRA4.3, features = genes, raster = FALSE, 
+DoHeatmap(uRA4.3, features = genes, raster = FALSE, 
 	group.colors = c("#00C08B", "#00B4F0", "#C77CFF"))
 
 ###########################
@@ -66,7 +66,7 @@ DoHeatmap(ueRA4.3, features = genes, raster = FALSE,
 ###########################
 
 ## Calculate the % of total MBCs contained in cluster 3.1
-p = round(table(Idents(ueRA4.1)) / sum(table(Idents(ueRA4.1))
+p = round(table(Idents(uRA4.1)) / sum(table(Idents(uRA4.1))
 
 slices = c(p[5], 100 - p[5])
 pie(slices, labels = NA, col = c("lightblue3", "lightgray"), clockwise = TRUE, 
